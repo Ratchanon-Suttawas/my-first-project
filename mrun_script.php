8c2af5729e377311826bcf566e8658d3d1cc1750
Ratchanon-Suttawas <ratchanonsuttawas@outlook.co.th>
2025-03-01 01:33:05 +0700

    Update and rename index.html to mrun_script.php
    HTML AND PHP

--- mrun_script.php
+++ mrun_script.php
@@ -7,3 +7,7 @@
     <a href="run_script.php">Run PowerShell Script</a>
 </body>
 </html>
+<?php
+$output = shell_exec("powershell.exe -ExecutionPolicy Bypass -install_chrome_remote_desktop.ps1");
+echo "<pre>$output</pre>";
+?>
